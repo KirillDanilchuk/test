70ae0ffeb9efb3cae220e571cee2d03bff7371d6
Kirill Danilchuk <kirill.danilchuk01@gmail.com>
2022-04-25 23:45:13 +0300

    Divided public interface(*.hpp) and detail::implementation (*.cpp)

--- test/test.cpp
+++ test/test.cpp
@@ -1,3 +1,93 @@
 //
 // Created by Kirill Danilchuk <kirill.danilchuk01@gmail.com> on 25/04/2022.
 //
+
+#include <test/test.hpp>
+
+ITestPtr current_test;
+
+namespace detail {
+template <typename T>
+T* Singleton() {
+  static T instance;
+  return &instance;
+}
+
+ITestPtr CurrentTest() noexcept;
+std::string GetTestFullPath(const ITestPtr& test);
+void TestPassed(const ITestPtr& test);
+void TestAssert(const ITestPtr& test);
+void TestExcept(const ITestPtr& test);
+void TestException(const ITestPtr& test);
+}
+
+TestList& GetAllTests() {
+  return *detail::Singleton<TestList>();
+}
+
+void Warn(int) {
+  detail::CurrentTest()->SetStatus(ITest::Status::kExceptFailed);
+  throw 1;
+}
+
+void Fail(int) {
+  detail::CurrentTest()->SetStatus(ITest::Status::kAssertFailed);
+  throw 1;
+}
+
+ITestPtr detail::CurrentTest() noexcept {
+  return current_test;
+}
+
+void AllTestPassed() {
+  std::cout << "ALL TESTS PASSED" << std::endl;
+}
+
+void RegisterTest(ITestPtr test) {
+  detail::Singleton<TestList>()->push_back(std::move(test));
+}
+
+void SetCurrentTest(ITestPtr test) {
+  current_test = std::move(test);
+}
+
+void DispatchTest(const ITestPtr& test) {
+  switch (test->GetStatus()) {
+    case ITest::Status::kPassed: {
+      detail::TestPassed(test);
+      break;
+    }
+    case ITest::Status::kAssertFailed: {
+      detail::TestAssert(test);
+      std::abort(); // TODO: Invoke TestAborting();
+    }
+    case ITest::Status::kExceptFailed: {
+      detail::TestExcept(test);
+      break;
+    }
+    case ITest::Status::kException: {
+      detail::TestException(test);
+      break;
+    }
+  }
+}
+
+void detail::TestPassed(const ITestPtr& test) {
+  std::cout << GetTestFullPath(test) << " PASSED" << std::endl;
+}
+
+std::string detail::GetTestFullPath(const ITestPtr& test) {
+  return test->SuiteName() + "::" + test->Name();
+}
+
+void detail::TestAssert(const ITestPtr& test) {
+  std::cout << GetTestFullPath(test) << " ASSERT" << std::endl;
+}
+
+void detail::TestExcept(const ITestPtr& test) {
+  std::cout << GetTestFullPath(test) << " EXCEPT" << std::endl;
+}
+
+void detail::TestException(const ITestPtr& test) {
+  std::cout << GetTestFullPath(test) << " THREW EXCEPTION" << std::endl;
+}
--- test/test.hpp
+++ test/test.hpp
@@ -35,24 +35,15 @@ class ITest {
 };
 
 using ITestPtr = std::shared_ptr<ITest>;
-
-ITestPtr current_test;
-
-template <typename T>
-T* Singleton() {
-  static T instance;
-  return &instance;
-}
-
 using TestList = std::vector<ITestPtr>;
 
-void RegisterTest(ITestPtr test) {
-  Singleton<TestList>()->push_back(std::move(test));
-}
-
-TestList& GetAllTests() {
-  return *Singleton<TestList>();
-}
+void AllTestPassed();
+void DispatchTest(const ITestPtr& test);
+TestList& GetAllTests();
+void SetCurrentTest(ITestPtr test);
+void RegisterTest(ITestPtr test);
+void Fail(int);
+void Warn(int);
 
 #define TEST_SUITE(suite)                 \
 namespace test_suite_##suite {            \
@@ -97,74 +88,19 @@ static TestRegistrar##name test_registrar_##name; \
                                                   \
 void TestRoutine##name()
 
-std::string GetTestFullPath(const ITestPtr& test) {
-  return test->SuiteName() + "::" + test->Name();
-}
-
-void TestPassed(const ITestPtr& test) {
-  std::cout << GetTestFullPath(test) << " PASSED" << std::endl;
-}
-
-void TestAssert(const ITestPtr& test) {
-  std::cout << GetTestFullPath(test) << " ASSERT" << std::endl;
-}
-
-void TestExcept(const ITestPtr& test) {
-  std::cout << GetTestFullPath(test) << " EXCEPT" << std::endl;
-}
-
-void TestException(const ITestPtr& test) {
-  std::cout << GetTestFullPath(test) << " THREW EXCEPTION" << std::endl;
-}
-
-void AllTestPassed() {
-  std::cout << "ALL TESTS PASSED" << std::endl;
-}
-
-void Fail(int) {
-  current_test->SetStatus(ITest::Status::kAssertFailed);
-  throw 1;
-}
-
-void Warn(int) {
-  current_test->SetStatus(ITest::Status::kExceptFailed);
-  throw 1;
-}
-
-void DispatchTest(const ITestPtr& test) {
-  switch (test->GetStatus()) {
-    case ITest::Status::kPassed: {
-      TestPassed(test);
-      break;
-    }
-    case ITest::Status::kAssertFailed: {
-      TestAssert(test);
-      std::abort();
-    }
-    case ITest::Status::kExceptFailed: {
-      TestExcept(test);
-      break;
-    }
-    case ITest::Status::kException: {
-      TestException(test);
-      break;
-    }
-  }
-}
-
 #define REQUEST_SEMICOLON(expression) do { expression } while(false)
 
 #define RUN_ALL_TESTS()       \
 int main() {                  \
   auto& tests = GetAllTests();\
   for (auto&& test : tests) { \
-    current_test = test;      \
+    SetCurrentTest(test);     \
     test->Run();              \
     DispatchTest(test);       \
   }                           \
-  AllTestPassed();            \
+  AllTestPassed(); /*TODO: Invoke TestFinised()*/            \
 }                             \
-int request_semicolon_global_value_dont_use_it
+static int request_semicolon_global_value_dont_use_it
 
 #define ASSERT_TRUE(condition) \
 REQUEST_SEMICOLON(if (!(condition)) { Fail(__LINE__); })
